e29f6c88af6cda3867fda57a128ba496d87f33b4
D3NJ1 <139505162+d3nj1h4x@users.noreply.github.com>
2025-02-05 06:15:08 +0600

    Create config.js

---
+++ source/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  token: {
+    ARINO:
+      "MTMyMTU5NTE3NDA1NjM2MjExMQ.GQlb5k.kpn6E6hq8idJi9nDC5c2J5JW2dazfLD0-lZpLw",
+  },
+  tokenTopgg: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkwNDMxNzE0MTg2NjY0NzU5MiIsImJvdCI6dHJ1ZSwiaWF0IjoxNjczNTI3OTYzfQ.WwA0KXh_nAQcBmR7BPqgLGyElYZheTQmguQfA2F6aNc",
+  mongoURL: {
+    RY4N: "mongodb+srv://d3nj1x77:d3nj1x77@d3nj1x77.ljpwl0l.mongodb.net/?retryWrites=true&w=majority",
+  },
+  clusterMode: "DEVELOPMENT",
+  def_prefix: "+",
+  vote_bypass: ["1085376019445321829", "990587571690168321"],
+  vote: "https://top.gg/bot/1321595174056362111/vote",
+  developers: ["990587571690168321", "1085376019445321829"],
+  managers: ["1085376019445321829", "990587571690168321"],
+  support: "https://discord.gg/W2GheK3F9m",
+  supportId: "1092123729401745510",
+  invite: "https://discord.com/oauth2/authorize?client_id=1321595174056362111&permissions=8&scope=bot%20applications.commands",
+  color: "#2f3136",
+  webhook: {
+    guildAdd: `https://discord.com/api/webhooks/1261781112808538282/_zdRHg9Vkw602UYVNtTQnBz2dX_kCsprEcMwh-fO6rx4xFcChDqbxT0oCwCmAYcH3SvP`,
+    guildRemove: `https://discord.com/api/webhooks/1261781112808538282/_zdRHg9Vkw602UYVNtTQnBz2dX_kCsprEcMwh-fO6rx4xFcChDqbxT0oCwCmAYcH3SvP`,
+    commandRun: `https://discord.com/api/webhooks/1261781112808538282/_zdRHg9Vkw602UYVNtTQnBz2dX_kCsprEcMwh-fO6rx4xFcChDqbxT0oCwCmAYcH3SvP`,
+  },
+  LOGGINGS: {
+    guildAdd:
+      "https://discord.com/api/webhooks/1261781112808538282/_zdRHg9Vkw602UYVNtTQnBz2dX_kCsprEcMwh-fO6rx4xFcChDqbxT0oCwCmAYcH3SvP",
+    guildRemove:
+      "https://discord.com/api/webhooks/1261781112808538282/_zdRHg9Vkw602UYVNtTQnBz2dX_kCsprEcMwh-fO6rx4xFcChDqbxT0oCwCmAYcH3SvP",
+    commandRun:
+      "https://discord.com/api/webhooks/1261781112808538282/_zdRHg9Vkw602UYVNtTQnBz2dX_kCsprEcMwh-fO6rx4xFcChDqbxT0oCwCmAYcH3SvP",
+  },
+  nodes: [
+    {
+      name: `A͏R͏I͏N͏O͏`,
+      url: `lavalink_v4.muzykant.xyz:80`,
+      auth: `https://discord.gg/v6sdrD9kPh`,
+      secure: false,
+    },
+  ],
+};
